3892dfe3c1cb696edd7442c4875edd341a10bea6
narek <narek.hakobyan.07@gmail.com>
2019-12-28 01:27:06 +0400

    fix package name

--- app_helper_generator/lib/builder.dart
+++ app_helper_generator/lib/builder.dart
@@ -10,7 +10,7 @@
 /// This library is **not** intended to be imported by typical end-users unless
 /// you are creating a custom compilation pipeline. See documentation for
 /// details, and `build.yaml` for how these builders are configured by default.
-library rexi_app_codegen.builder;
+library app_helper_generator.builder;
 
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
